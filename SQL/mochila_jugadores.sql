69a5f052a86f57d2801ec0500c1fd2453947b63a
marbeiro <mbeiro@fi.uba.ar>
2021-05-11 14:12:05 -0300

    Dataset de jugadores para el problema de la mochila.

--- SQL/mochila_jugadores.sql
+++ SQL/mochila_jugadores.sql
@@ -4,3 +4,12 @@ precio INT,
 tecnica NUMERIC(4,2),
 fecha_nac DATE
 );
+
+INSERT INTO Jugadores
+VALUES
+('Lionel Messi',80,9.80,'1987-06-24'),
+('Cristiano Ronaldo',75,9.70,'1985-02-05'),
+('Luis Suárez',45,8.70,'1987-01-24'),
+('Ciro Immobile',30,5.00,'1990-02-20'),
+('Karim Benzema',36,7.20,'1987-12-19'),
+('Paul Pogba',40,8.30,'1993-03-15');
