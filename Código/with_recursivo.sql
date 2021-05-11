b63e8202418e0b75e964315506e9f5bbb1804bef
marbeiro <mbeiro@fi.uba.ar>
2021-05-11 15:16:49 -0300

    dataset de vuelos y código de WITH RECURSIVO

--- Código/with_recursivo.sql
+++ Código/with_recursivo.sql
@@ -1,5 +1,6 @@
 -- Este dataset utiliza la tabla Jugadores incluída
--- en el script mochila_jugadores.sql
+-- en el script mochila_jugadores.sql,
+-- y la tabla de Vuelos en el script vuelos.sql
 
 -- OBJETIVO: Armar un equipo de jugadores
 -- con la mayor técnica posible sin gastar más de 140
@@ -15,3 +16,15 @@ WITH RECURSIVE Mochila(tecnica_total, monto, equipo) AS
 
 SELECT *
 FROM Mochila WHERE tecnica_total >= ALL (SELECT m2.tecnica_total FROM Mochila m2);
+
+-- OBJETIVO: Hallar todas las ciudades
+-- alcanzables desde París.
+WITH RECURSIVE DestAlcanzables(destino_alc) AS
+    (VALUES ('París')
+    UNION
+    (SELECT DISTINCT v.destino
+     FROM DestAlcanzables d, Vuelos v
+     WHERE d.destino_alc = v.origen)
+    )
+SELECT *
+FROM DestAlcanzables;
